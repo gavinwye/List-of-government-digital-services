71c3f7402e8a04c82eaa83f765b122cf5e6e0d88
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2021-04-20 15:43:21 +0100

    Expose data to the register path, and use it in a template

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const services =  require('./data/services.json');
 
 // Add your routes here - above the module.exports line
+
+router.get('/register', (req, res) => {
+  res.render('register',
+    {
+      data: {
+        services: services,
+      }
+    }
+  )
+})
 
 module.exports = router

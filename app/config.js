e550d805dd5af2f3dc49749d3cb25bd1eb7ad607
Gavin Wye <github@gavin.is>
2021-04-21 16:25:09 +0200

    Changed the name from register to list

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Register of services',
+  serviceName: 'List of government digital services',
 
   // Default port that prototype runs on
   port: '3000',
